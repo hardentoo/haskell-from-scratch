5cbc2d4607c0a6e06f007a08f27198ca815aa3c1
AndrewRademacher <andrewrademacher@gmail.com>
2014-12-07 16:23:07 -0600

    Removed old files.

--- 02-Fibonacci/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-main :: IO ()
-main = do
-    putStrLn $ "fib  4 = " ++ show (fib 4) ++ ", should be 3"
-    putStrLn $ "fib  6 = " ++ show (fib 6) ++ ", should be 8"
-    putStrLn $ "fib 13 = " ++ show (fib 13) ++ ", should be 233"
-
--- | Begin work below this line.
